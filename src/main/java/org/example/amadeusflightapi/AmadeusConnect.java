8ebaa85d9fdbfd4ff904dbe72879bac9974c5dbf
brianpalmer <brianpalmer1321@icloud.com>
2024-09-08 19:04:43 -0400

    update

---
+++ src/main/java/org/example/amadeusflightapi/AmadeusConnect.java
@@ -0,0 +1,23 @@
+package org.example.amadeusflightapi;
+
+
+import com.amadeus.Amadeus;
+import com.amadeus.Params;
+import com.amadeus.referenceData.Locations;
+import com.amadeus.resources.Location;
+import com.amadeus.exceptions.ResponseException;
+enum AmadeusConnect {
+    INSTANCE;
+    private Amadeus amadeus;
+    private AmadeusConnect() {
+        this.amadeus = Amadeus
+
+                .builder("06TOKZfxqSPMpu32WnIGgIXRlk7aHV8x", "2YzRkNj9SeJUSl9S")
+                .build();
+    }
+    public Location[] location(String keyword) throws ResponseException {
+        return amadeus.referenceData.locations.get(Params
+                .with("keyword", keyword)
+                .and("subType", Locations.AIRPORT));
+    }
+}
---
+++ src/main/java/org/example/amadeusflightapi/ApiController.java
@@ -0,0 +1,23 @@
+package org.example.amadeusflightapi;
+
+
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.Location;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+@RequestMapping(value="/api")
+public class ApiController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World Travel!";
+    }
+
+    @GetMapping("/locations")
+    public Location[] locations(@RequestParam(required=true) String keyword) throws ResponseException {
+        return AmadeusConnect.INSTANCE.location(keyword);
+    }
+}
