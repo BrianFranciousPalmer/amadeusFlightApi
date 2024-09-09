c000520e8619a47219dd15e69992f6559f60596d
brianpalmer <brianpalmer1321@icloud.com>
2024-09-09 10:53:57 -0400

    Step 6 complete. Added flight searching. Test with http://localhost:8080/api/flights?origin=LON&destination=NYC&departDate=2024-11-15&returnDate=2024-11-17&adults=3

--- src/main/java/org/example/amadeusflightapi/AmadeusConnect.java
+++ src/main/java/org/example/amadeusflightapi/AmadeusConnect.java
@@ -1,11 +1,12 @@
 package org.example.amadeusflightapi;
 
-
 import com.amadeus.Amadeus;
 import com.amadeus.Params;
 import com.amadeus.referenceData.Locations;
+import com.amadeus.resources.FlightOfferSearch;
 import com.amadeus.resources.Location;
 import com.amadeus.exceptions.ResponseException;
+
 enum AmadeusConnect {
     INSTANCE;
     private Amadeus amadeus;
@@ -19,5 +20,14 @@ enum AmadeusConnect {
         return amadeus.referenceData.locations.get(Params
                 .with("keyword", keyword)
                 .and("subType", Locations.AIRPORT));
+    }
+    public FlightOfferSearch[] flights(String origin, String destination, String departDate, String adults, String returnDate) throws ResponseException {
+        return amadeus.shopping.flightOffersSearch.get(
+                Params.with("originLocationCode", origin)
+                        .and("destinationLocationCode", destination)
+                        .and("departureDate", departDate)
+                        .and("returnDate", returnDate)
+                        .and("adults", adults)
+                        .and("max", 3));
     }
 }
--- src/main/java/org/example/amadeusflightapi/ApiController.java
+++ src/main/java/org/example/amadeusflightapi/ApiController.java
@@ -2,6 +2,7 @@ package org.example.amadeusflightapi;
 
 
 import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.FlightOfferSearch;
 import com.amadeus.resources.Location;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,5 +20,15 @@ public class ApiController {
     @GetMapping("/locations")
     public Location[] locations(@RequestParam(required=true) String keyword) throws ResponseException {
         return AmadeusConnect.INSTANCE.location(keyword);
+    }
+
+    @GetMapping("/flights")
+    public FlightOfferSearch[] flights(@RequestParam(required=true) String origin,
+                                       @RequestParam(required=true) String destination,
+                                       @RequestParam(required=true) String departDate,
+                                       @RequestParam(required=true) String adults,
+                                       @RequestParam(required = false) String returnDate)
+            throws ResponseException {
+        return AmadeusConnect.INSTANCE.flights(origin, destination, departDate, adults, returnDate);
     }
 }
